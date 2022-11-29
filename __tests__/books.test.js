71f7cfdbc727f136789e0f8f48cf4ad74bad9217
Lex <106777728+Lexus-Banton@users.noreply.github.com>
2022-11-28 20:26:34 -0800

    books should return list of books test done

---
+++ __tests__/books.test.js
@@ -0,0 +1,23 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('book routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('/books should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(7);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(Number),
+    });
+  });
+  expect(1).toEqual(1);
+});
+afterAll(() => {
+  pool.end();
+});
