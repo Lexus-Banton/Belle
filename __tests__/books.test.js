ddb89e8468ada5f3d77e4052391dd0486a001954
Lex <106777728+Lexus-Banton@users.noreply.github.com>
2022-11-28 21:12:13 -0800

    book detail test done

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -11,6 +11,21 @@ describe('book routes', () => {
   it('/books should return a list of books', async () => {
     const res = await request(app).get('/books');
     expect(res.body.length).toEqual(8);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      release: expect.any(Number),
+      genre: expect.any(String),
+    });
+
+    it('/books/1 should return a book detail with authors', async () => {
+      const res = await request(app).get('/books/1');
+      expect(res.body).toEqual({
+        name: expect.any(String),
+        release: expect.any(Number),
+        authors: expect.any(Array),
+      });
+    });
   });
 
   afterAll(() => {
